707e3158ad72d4aba4e03c9f99978787090547e1
Burak Özmen <brkzmn.wd@gmail.com>
2022-04-05 23:21:10 +0200

    modified

--- Week1/exercise-1.js
+++ Week1/exercise-1.js
@@ -1,51 +1,81 @@
+import util from "util";
 import mysql from "mysql";
+
 const connection = mysql.createConnection({
   host: "localhost",
   user: "hyfuser",
   password: "hyfpassword",
 });
 
-connection.connect();
+const execQuery = util.promisify(connection.query.bind(connection));
 
-const connectionHandler = (error, results) => {
-  if (error) throw error;
-  console.log(results);
+const createInviteeTableQuery = `
+  CREATE TABLE Invitee (
+    invitee_no INT, 
+    invitee_name varchar(50), 
+    invited_by varchar(50)
+  )`;
+
+const insertInviteeQuery = `
+  INSERT INTO Invitee VALUES 
+    (1, "John Doe", "Gordon Norman"), 
+    (2, "Thomas Toe", "Samuel Serif"), 
+    (3, "Pelican Steve", "Gunther Beard"), 
+    (4, "Jake Weary", "Russell Sprout"), 
+    (5, "Fletch Skinner", "Will Barrow")
+  `;
+
+const createRoomTableQuery = `
+  CREATE TABLE Room (
+    room_no INT, 
+    room_name varchar(50), 
+    floor_number INT
+  )`;
+
+const insertRoomQuery = `
+  INSERT INTO Room VALUES 
+    (601, "The ABC Room", 6), 
+    (602, "The Growth Room", 6), 
+    (603, "The Freedom Room", 6), 
+    (701, "The Orion Room", 7), 
+    (702, "The Focus Room", 7)
+  `;
+
+const createMeetingTableQuery = `
+  CREATE TABLE Meeting (
+    meeting_no INT, 
+    meeting_title text, 
+    starting_time DATETIME, 
+    ending_time DATETIME, 
+    room_no INT)
+  `;
+
+const insertMeetingQuery = `
+  INSERT INTO Meeting VALUES 
+    (10, 'Meeting About HTML', '2022-03-23 12:30:00', '2022-03-23 15:30:00', 601), 
+    (11, 'Meeting About CSS', '2022-03-24 12:30:00', '2022-03-24 15:30:00', 603), 
+    (12, 'Meeting About JavaScript', '2022-03-24 15:30:00', '2022-03-24 17:30:00', 604), 
+    (14, 'Meeting About NodeJS', '2022-03-25 11:30:00', '2022-03-25 16:30:00', 701), 
+    (15, 'Meeting About React', '2022-03-26 12:30:00', '2022-03-26 14:30:00', 702)
+  `;
+
+const seedDatabase = async () => {
+  connection.connect();
+  try {
+    await execQuery("DROP DATABASE IF EXISTS meetup");
+    await execQuery("CREATE DATABASE meetup");
+    await execQuery("USE meetup");
+    await execQuery(createInviteeTableQuery);
+    await execQuery(createRoomTableQuery);
+    await execQuery(createMeetingTableQuery);
+    await execQuery(insertInviteeQuery);
+    await execQuery(insertRoomQuery);
+    await execQuery(insertMeetingQuery);
+  } catch (error) {
+    console.error(error);
+  }
+
+  connection.end();
 };
 
-const createInviteeTableQuery =
-  "CREATE TABLE Invitee (invitee_no INT, invitee_name varchar(50), invited_by varchar(50))";
-
-const insertInviteeQuery =
-  'INSERT INTO Invitee VALUES (1, "John Doe", "Gordon Norman"), (2, "Thomas Toe", "Samuel Serif"), (3, "Pelican Steve", "Gunther Beard"), (4, "Jake Weary", "Russell Sprout"), (5, "Fletch Skinner", "Will Barrow")';
-
-const createRoomTableQuery =
-  "CREATE TABLE Room (room_no INT , room_name varchar(50), floor_number INT)";
-
-const insertRoomQuery =
-  'INSERT INTO Room VALUES (601, "The ABC Room", 6), (602, "The Growth Room", 6), (603, "The Freedom Room", 6), (701, "The Orion Room", 7), (702, "The Focus Room", 7)';
-
-const createMeetingTableQuery =
-  "CREATE TABLE Meeting (meeting_no INT, meeting_title text, starting_time DATETIME, ending_time DATETIME, room_no INT)";
-
-const insertMeetingQuery =
-  "INSERT INTO Meeting VALUES (10, 'Meeting About HTML', '2022-03-23 12:30:00', '2022-03-23 15:30:00', 601), (11, 'Meeting About CSS', '2022-03-24 12:30:00', '2022-03-24 15:30:00', 603), (12, 'Meeting About JavaScript', '2022-03-24 15:30:00', '2022-03-24 17:30:00', 604), (14, 'Meeting About NodeJS', '2022-03-25 11:30:00', '2022-03-25 16:30:00', 701), (15, 'Meeting About React', '2022-03-26 12:30:00', '2022-03-26 14:30:00', 702)";
-
-connection.query("DROP DATABASE IF EXISTS meetup", connectionHandler);
-
-connection.query("CREATE DATABASE meetup", connectionHandler);
-
-connection.query("USE meetup", connectionHandler);
-
-connection.query(createInviteeTableQuery, connectionHandler);
-
-connection.query(createRoomTableQuery, connectionHandler);
-
-connection.query(createMeetingTableQuery, connectionHandler);
-
-connection.query(insertInviteeQuery, connectionHandler);
-
-connection.query(insertRoomQuery, connectionHandler);
-
-connection.query(insertMeetingQuery, connectionHandler);
-
-connection.end();
+seedDatabase();
--- Week1/exercise-2.js
+++ Week1/exercise-2.js
@@ -1,3 +1,4 @@
+import util from "util";
 import mysql from "mysql";
 
 const connection = mysql.createConnection({
@@ -7,61 +8,42 @@ const connection = mysql.createConnection({
   database: "world",
 });
 
-connection.connect();
+const execQuery = util.promisify(connection.query.bind(connection));
 
-const connectionHandler = (error, results) => {
-  if (error) throw error;
-  console.log(results);
+const seedDatabase = async () => {
+  connection.connect();
+  try {
+    await execQuery(
+      `SELECT Name as 'City - greater than 8 mln' FROM city WHERE Population > 8000000`
+    );
+    await execQuery(
+      `SELECT Name as 'country - with land' FROM country WHERE Name LIKE '%land%'`
+    );
+    await execQuery(
+      `SELECT Name as 'city - between 500k - 1 mln' FROM city WHERE Population BETWEEN 500000 AND 1000000`
+    );
+    await execQuery(
+      `SELECT NAME as 'country in Europe' FROM country WHERE Continent = 'Europe'`
+    );
+    await execQuery(`SELECT * FROM country ORDER BY SurfaceArea DESC`);
+    await execQuery(
+      `SELECT Name as 'city in NLD' FROM city WHERE CountryCode = 'NLD'`
+    );
+    await execQuery(
+      `SELECT Population as 'Population of Rotterdam' FROM city WHERE Name = 'Rotterdam'`
+    );
+    await execQuery(
+      `SELECT Name as 'large country' FROM country ORDER BY SurfaceArea DESC LIMIT 10`
+    );
+    await execQuery(`SELECT * FROM city ORDER BY Population DESC LIMIT 10`);
+    await execQuery(
+      `SELECT SUM(Population) AS 'World Population' FROM country`
+    );
+  } catch (error) {
+    console.error(error);
+  }
+
+  connection.end();
 };
 
-connection.query(
-  "SELECT Name as 'City - greater than 8 mln' FROM city WHERE Population > 8000000",
-  connectionHandler
-);
-
-connection.query(
-  `SELECT Name as 'country - with land' FROM country WHERE Name LIKE '%land%'`,
-  connectionHandler
-);
-
-connection.query(
-  "SELECT Name as 'city - between 500k - 1 mln' FROM city WHERE Population BETWEEN 500000 AND 1000000",
-  connectionHandler
-);
-
-connection.query(
-  "SELECT NAME as 'country in Europe' FROM country WHERE Continent = 'Europe'",
-  connectionHandler
-);
-
-connection.query(
-  "SELECT * FROM country ORDER BY SurfaceArea DESC",
-  connectionHandler
-);
-
-connection.query(
-  "SELECT Name as 'city in NLD' FROM city WHERE CountryCode = 'NLD'",
-  connectionHandler
-);
-
-connection.query(
-  "SELECT Population as 'Population of Rotterdam' FROM city WHERE Name = 'Rotterdam'",
-  connectionHandler
-);
-
-connection.query(
-  "SELECT Name as 'large country' FROM country ORDER BY SurfaceArea DESC LIMIT 10",
-  connectionHandler
-);
-
-connection.query(
-  "SELECT * FROM city ORDER BY Population DESC LIMIT 10",
-  connectionHandler
-);
-
-connection.query(
-  "SELECT SUM(Population) AS 'World Population' FROM country",
-  connectionHandler
-);
-
-connection.end();
+seedDatabase();
